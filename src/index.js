23e00b9d6d1bd5345d1b7ea347ce501c6e70a54e
Ismary Salges <ismarysalges@gmail.com>
2018-06-01 09:11:59 -0400

    Hice el test, exitoso

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 window.onload = () => {
+  alert("Tienes 23 mensajes sin leer");
+  const btn_hide = document.getElementById("instru_app");
+
+    btn_hide.addEventListener("click", () => {
+      document.getElementsByClassName("hide")[0].classList.add("text_show");
+      document.getElementsByClassName("bye")[0].classList.remove("bye");
+})
   /*Evento del primer click cifrar */
   const  btn = document.getElementById("btn_cifrar");
     btn.addEventListener("click",  () => {
@@ -10,7 +17,7 @@ window.onload = () => {
   const conx = window.cipher.encode(str,desplaz);
     document.getElementById("msj_descifrar").innerHTML = conx
   })
-       // CAMBIAR LAS VARIABLES POR CONST // !!     
+   
   /*Evento del primer click descifrar */
   const  btnDos = document.getElementById("btn_descifrar");
     btnDos.addEventListener("click",  () => {
